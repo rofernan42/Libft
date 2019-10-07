855a03ffccfb7af538873b82651cec9684edecb4
Romain FERNANDEZ <rofernan@e2r9p8.42.fr>
2019-10-07 16:32:48 +0200

    push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 15:29:50 by rofernan          #+#    #+#             */
-/*   Updated: 2019/10/07 16:09:16 by rofernan         ###   ########.fr       */
+/*   Updated: 2019/10/07 16:30:40 by rofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
 
 int		main(int argc, char **argv)
 {
@@ -34,5 +36,9 @@ int		main(int argc, char **argv)
 	printf("ft_isalnum: %d\n\n", ft_isalnum(argv[1][0]));
 	printf("isascii:    %d\n", isascii(argv[1][0]));
 	printf("ft_isascii: %d\n\n", ft_isascii(argv[1][0]));
+	printf("isprint:    %d\n", isprint(argv[1][0]));
+	printf("ft_isprint: %d\n\n", ft_isprint(argv[1][0]));
+	printf("toupper:    %c\n", toupper(argv[1][0]));
+	printf("ft_toupper: %c\n\n", ft_toupper(argv[1][0]));
 	return (0);
 }
