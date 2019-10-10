22132620b395a9d5fbe20cc6f2b28cf5177c8916
Romain FERNANDEZ <rofernan@e2r9p16.42.fr>
2019-10-10 18:33:42 +0200

    update

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 16:58:34 by rofernan          #+#    #+#             */
-/*   Updated: 2019/10/10 18:10:04 by rofernan         ###   ########.fr       */
+/*   Updated: 2019/10/10 18:32:19 by rofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,17 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(void *))
 {
 	t_list *new_lst;
 	t_list *tmp;
+	t_list *tmp_new;
 
 	tmp = lst;
+	tmp = f(tmp->content);
+	new_lst = ft_lstnew(tmp->content);
+	while (tmp)
+	{
+		tmp = tmp->next;
+		tmp = f(tmp->content);
+		new_lst = ft_lstnew(tmp->content);
+		tmp_new = tmp_new->next;
+	}
 	return (new_lst);
 }
