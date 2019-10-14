9bf3f5b3a7cc6f1ad57a1813357cfdb4c6ea5ee3
Romain FERNANDEZ <rofernan@e3r9p11.42.fr>
2019-10-14 18:49:05 +0200

    modif strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 17:09:28 by rofernan          #+#    #+#             */
-/*   Updated: 2019/10/11 12:26:24 by rofernan         ###   ########.fr       */
+/*   Updated: 2019/10/14 18:45:46 by rofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 static	int	check_set(char c, char const *set)
 {
-	while (*set)
+	int i;
+
+	i = 0;
+	while (set[i])
 	{
-		if (*set == c)
+		if (set[i] == c)
 			return (1);
-		set++;
+		i++;
 	}
 	return (0);
 }
@@ -33,18 +36,18 @@ char		*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	j = 0;
 	k = 0;
-	if (!s1)
+	if (!s1[i])
 		return (NULL);
 	while (check_set(s1[i], set))
 		i++;
-	while (s1[i + j])
-		j++;
-	j--;
+	if ((j = ft_strlen(&s1[i])) > 0)
+		j--;
 	while (check_set(s1[i + j], set))
 		j--;
+	j++;
 	if (!(str = malloc(sizeof(*str) * j)))
 		return (NULL);
-	while (k <= j)
+	while (k < j)
 		str[k++] = s1[i++];
 	str[k] = '\0';
 	return (str);
